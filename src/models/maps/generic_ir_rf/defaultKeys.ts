c32cae48dc3264f0e7ea2223f28cc28151acd497
Felipe Santos <felipecassiors@gmail.com>
2025-05-12 21:37:34 -0300

    Add default keys and sources for generic ir/rf

--- src/models/maps/generic_ir_rf/defaultKeys.ts
+++ src/models/maps/generic_ir_rf/defaultKeys.ts
@@ -1,6 +1,10 @@
 import { IElementConfig } from '../../interfaces';
+import { androidTVDefaultKeys } from '../android_tv';
 
 /**
- *
+ * Copied from Android TV map to simplify configuration.
  */
-export const genericIRRFDefaultKeys: IElementConfig[] = [];
+export const genericIRRFDefaultKeys: IElementConfig[] = androidTVDefaultKeys.filter(
+    (key) =>
+        key.tap_action === undefined || key.tap_action.action === 'key'
+);
--- src/models/maps/generic_ir_rf/defaultSources.ts
+++ src/models/maps/generic_ir_rf/defaultSources.ts
@@ -1,6 +1,15 @@
 import { IElementConfig } from '../../interfaces';
+import { androidTVDefaultSources } from '../android_tv';
 
 /**
- *
+ * Copied from Android TV map to simplify configuration.
  */
-export const genericIRRFDefaultSources: IElementConfig[] = [];
+export const genericIRRFDefaultSources: IElementConfig[] = androidTVDefaultSources.map(
+    (key) => ({
+        ...key,
+        tap_action: {
+            action: 'key',
+            key: `SOURCE_${key.name.toUpperCase()}`,
+        },
+    })
+)
