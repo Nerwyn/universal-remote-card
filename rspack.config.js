b9c09d856d1bf6103fad3423ba3ca7fa43530e02
Nerwyn Singh <nerwyn1994@gmail.com>
2025-11-30 20:48:21 -0500

    update eslint and rspack configs to es module syntax

--- rspack.config.js
+++ rspack.config.js
@@ -1,6 +1,5 @@
-/* eslint-env es6 */
-const path = require('path');
-const { execSync } = require('child_process');
+import { defineConfig } from '@rspack/cli';
+import { execSync } from 'child_process';
 
 let env =
 	execSync('git branch --show-current').toString().trim() == 'main'
@@ -8,40 +7,42 @@ let env =
 		: 'development';
 env = 'production';
 
-module.exports = {
-	mode: env,
-	entry: {
-		main: './src/universal-remote-card.ts',
-	},
-	output: {
-		path: path.resolve(__dirname, './dist'),
-		filename: 'universal-remote-card.min.js',
-	},
-	resolve: {
-		extensions: ['.ts', '.tsx', '.js'],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				loader: 'ts-loader',
-			},
-			{
-				test: /\.(jsx?|tsx?)$/,
-				loader: 'minify-html-literals-loader',
-			},
-			{
-				test: /\.m?js/,
-				resolve: {
-					fullySpecified: false,
+export default defineConfig([
+	{
+		mode: env,
+		entry: {
+			main: './src/universal-remote-card.ts',
+		},
+		output: {
+			path: './dist',
+			filename: 'universal-remote-card.min.js',
+		},
+		resolve: {
+			extensions: ['.ts', '.tsx', '.js'],
+		},
+		module: {
+			rules: [
+				{
+					test: /\.tsx?$/,
+					loader: 'ts-loader',
 				},
-			},
-		],
+				{
+					test: /\.(jsx?|tsx?)$/,
+					loader: 'minify-html-literals-loader',
+				},
+				{
+					test: /\.m?js/,
+					resolve: {
+						fullySpecified: false,
+					},
+				},
+			],
+		},
+		performance: {
+			hints: false,
+			maxEntrypointSize: 512000,
+			maxAssetSize: 512000,
+		},
+		devtool: env == 'production' ? false : 'eval',
 	},
-	performance: {
-		hints: false,
-		maxEntrypointSize: 512000,
-		maxAssetSize: 512000,
-	},
-	devtool: env == 'production' ? false : 'eval',
-};
+]);
